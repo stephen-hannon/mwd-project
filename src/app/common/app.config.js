6caa43752a47446fa5ddc4c640005a8a13ce72f0
Stephen Hannon <stephenhannon4@gmail.com>
2020-09-30 14:33:56 -0400

    Add parse credentials

--- src/app/common/app.config.js
+++ src/app/common/app.config.js
@@ -1,5 +1,9 @@
-function init(/*ParseProvider*/) {
+function init(ParseProvider) {
     // parse credentials
+    var PARSE_APP_ID = 'QgA8eKudk3aNAqZazDzlhnCvoCyOMfok9fPqOW7Q';
+    var PARSE_JS_KEY = 'xFjscdHL6guWdW35IOvGsRv4LO3uBXlsQZxygQMu';
+    ParseProvider.initialize(PARSE_APP_ID, PARSE_JS_KEY);
+    ParseProvider.serverURL = 'https://parseapi.back4app.com'; 
 }
 
 angular
